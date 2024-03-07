a4a2a13f194f99bd385f4171c83184e30da9cb74
balmed <iwymed@gamil.com>
2024-03-07 15:39:04 -0800

    ajout web

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["18.204.16.219", "52.72.20.22"]
 
 
 def do_pack():
